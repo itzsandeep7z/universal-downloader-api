e340f32c05016a39cfaaed9a2dcf1fd8bba993cd
itzsandeep7z <sandeeppatel12100@gmail.com>
2026-01-29 15:20:20 +0530

    Update start.py

--- start.py
+++ start.py
@@ -6,8 +6,7 @@ import time
 def run_api():
     port = os.getenv("PORT", "8080")
     subprocess.run([
-        "uvicorn",
-        "main:app",
+        "uvicorn", "main:app",
         "--host", "0.0.0.0",
         "--port", port
     ])
@@ -15,11 +14,8 @@ def run_api():
 def run_bot():
     subprocess.run(["python", "bot.py"])
 
-t1 = threading.Thread(target=run_api)
-t2 = threading.Thread(target=run_bot)
-
-t1.start()
-t2.start()
+threading.Thread(target=run_api).start()
+threading.Thread(target=run_bot).start()
 
 while True:
     time.sleep(60)
